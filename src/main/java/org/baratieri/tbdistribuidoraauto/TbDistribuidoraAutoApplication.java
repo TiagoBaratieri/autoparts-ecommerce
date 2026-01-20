506d34338b810627b2ece89e3308e4ea831ad95d
Tiago Baratieri <tiagobaratieri97@gmail.com>
2026-01-20 11:32:14 -0300

    chore: initial commit structure

---
+++ src/main/java/org/baratieri/tbdistribuidoraauto/TbDistribuidoraAutoApplication.java
@@ -0,0 +1,13 @@
+package org.baratieri.tbdistribuidoraauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbDistribuidoraAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbDistribuidoraAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/baratieri/tbdistribuidoraauto/TbDistribuidoraAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baratieri.tbdistribuidoraauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbDistribuidoraAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
